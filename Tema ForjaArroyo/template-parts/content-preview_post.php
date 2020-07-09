fb038ea39dc530e3b1bcf613d3c51b000c532dfb
alsofa95 <alsofa95@gmail.com>
2020-07-09 16:09:11 +0200

    Sombras en novedades

--- Tema ForjaArroyo/template-parts/content-preview_post.php
+++ Tema ForjaArroyo/template-parts/content-preview_post.php
@@ -2,7 +2,7 @@
 
 <!-- Articulo -->
 <div class="col-sm-3" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="thumbnail">
+    <div class="thumbnail shadow">
         <a href="<?php echo esc_url( get_permalink()); ?>">
             <img class="img-preview img-responsive img-rounded" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" width="400" height="300">
         </a>

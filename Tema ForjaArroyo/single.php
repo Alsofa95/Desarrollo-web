7120f12e219cf8418bb65f3f921430b4d2aba3a2
alsofa95 <alsofa95@gmail.com>
2020-07-20 15:31:46 +0200

    Change style category single

--- Tema ForjaArroyo/single.php
+++ Tema ForjaArroyo/single.php
@@ -51,10 +51,7 @@ $categories="";
 						if ( ! empty( $categories ) ) {
 							for($i = 0 ; $i < sizeof($categories);$i++){
 								echo '<a class="link_post" href="' . esc_url( get_category_link( $categories[$i]->term_id ) ) . '">' . esc_html( $categories[$i]->name ) . '</a> ';	
-								if(sizeof($categories) != $i+1) echo ' / ';
 							}
-							//print_r($categories);
-							
 						} 
 					?>
 					</h3>

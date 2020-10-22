be281b8b7707551734da16055d306582638f2e5b
alsofa95 <alsofa95@gmail.com>
2020-10-22 21:27:07 +0200

    Custom header image

--- Tema ForjaArroyo/template-parts/content-index_cartelera.php
+++ Tema ForjaArroyo/template-parts/content-index_cartelera.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<div class="jumbotron text-center text-white">
+<div class="jumbotron text-center text-white" style="background-image: url(<?php echo get_header_image(); ?>);">
     <h1 class="title-jumbotron w-100">Forja Arroyo</h1> 
     <p class="subtitle-jumbotron">Fabricación de Muebles de Forja Artesanal</p>
 </div>
